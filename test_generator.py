442b5797eee2f754da39753cccd80eedcc954db0
mariawood98 <63402347+mariawood98@users.noreply.github.com>
2021-01-20 11:47:45 +0000

    19/01/21

--- test_generator.py
+++ test_generator.py
@@ -46,11 +46,11 @@ def galaxyPhotons(x_centre,y_centre, radius):
 	photon_vals=[];
 	for i in range(x_centre-radius,x_centre+radius):
 		for j in range(y_centre-radius,y_centre+radius):
-			if i<(image_size*2) and i>=0 and j<(image_size) and j>=0: #only consider pixels within the image
+			if i<(image_size) and i>=0 and j<(image_size*2) and j>=0: #only consider pixels within the image
 				d=np.sqrt((i-x_centre)**2+(j-y_centre)**2)
 				if d<radius: 
 					photon_vals.append(basearray[i,j])
-			masktest[i,j]=1#this isn't working for some reason, whether inside if statement or not
+				masktest[i,j]=1
 	return(np.sum(photon_vals))
 
 #averages values of pixels between 1st and 2nd aperture, local bakcground mean
@@ -58,7 +58,7 @@ def localBackground(x_centre,y_centre, initialRadius, secondaryRadius):
 	bg_photon_vals=[];
 	for i in range(x_centre-secondaryRadius,x_centre+secondaryRadius):#outer x
 		for j in range(y_centre-secondaryRadius,y_centre+secondaryRadius):#outer y
-			if i<(image_size*2) and i>=0 and j<(image_size) and j>=0: #only consider pixels within the image!
+			if i<(image_size) and i>=0 and j<(image_size*2) and j>=0: #only consider pixels within the image!
 				d2=np.sqrt((i-x_centre)**2+(j-y_centre)**2)
 				if d2<secondaryRadius and d2>initialRadius: #only consider pixels between the two apertures
 					bg_photon_vals.append(basearray[i,j])
@@ -81,7 +81,7 @@ for i in range(len(base1d_sorted)):
 			loc = tempPixPos[j]
 			if masktest[loc[1],loc[0]] == 0: #if pixel is available to use 
 				galaxyBrightness=galaxyPhotons(loc[1],loc[0],initialRadius)
-				galaxyBackground = 0#localBackground(loc[1],loc[0],initialRadius, secondaryRadius)
+				galaxyBackground = localBackground(loc[1],loc[0],initialRadius, secondaryRadius)
 				galaxyCounts.append(galaxyBrightness-galaxyBackground)
 				print(tempPixVal)
 plt.figure()
