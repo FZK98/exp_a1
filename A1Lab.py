227af6f793de4a65c3fae8fd70c0465a3478c682
mariawood98 <63402347+mariawood98@users.noreply.github.com>
2021-01-20 15:54:08 +0000

    20/01/21

--- A1Lab.py
+++ A1Lab.py
@@ -61,39 +61,76 @@ while N > 0 :
     N -= yhistogram[muindex - 1]
     i +=1
     
-print('the pixel val N points away from the mean', xhistogram[muindex+i])
-print('pixel val 1 sig from the mean', xhistogram[np.abs(xhistogram - (popt[1]+ popt[2])).argmin()])
-print('pixel val 2 sig from the mean', xhistogram[np.abs(xhistogram - (popt[1]+ 2*popt[2])).argmin()])
-print('pixel val 2 sig from the mean', xhistogram[np.abs(xhistogram - (popt[1]+ 3*popt[2])).argmin()])
+#print('the pixel val N points away from the mean', xhistogram[muindex+i])
+#print('pixel val 1 sig from the mean', xhistogram[np.abs(xhistogram - (popt[1]+ popt[2])).argmin()])
+#print('pixel val 2 sig from the mean', xhistogram[np.abs(xhistogram - (popt[1]+ 2*popt[2])).argmin()])
+#print('pixel val 2 sig from the mean', xhistogram[np.abs(xhistogram - (popt[1]+ 3*popt[2])).argmin()])
 meanBackground = popt[1]
 sigmaBackground = popt[2]*1000 # this can be altered accordingly - used to determine if galaxy significant
 
 #0 value means pixel can be considered
 #1 value means pixel should be ignored
-mask = np.zeros((np.shape(data)[1],np.shape(data)[0])) #initiate a mask image (need to swap x and y round)
+mask = np.zeros((np.shape(data)[0],np.shape(data)[1])) #initiate a mask image (need to swap x and y round)
+
+#masking of foreground pixels including stars, bleeding and noisy edges
+for i in range (imagex):
+    for j in range(imagey):
+        #masking the borders
+        if i <= 200 or j <= 200 or j >= 2370 or i>= 4411: 
+            mask[i,j] = 1
+
+        #masking pixel values below 2 std devs from the mean 
+       # if mask[i][j] <= popt[1] + 2*popt[2]:
+       #     mask[i][j] = 1
+        #mask the central vertical bloom 
+        if 1425 <= j <= 1447:
+            mask[i,j] = 1
+
+        #mask the central brightest star
+        if 1230 <= j <= 1652 and 3000 <= i <= 3425:
+            mask[i,j] = 1
+
+        #masking 3 horizontal blooms (with rectangles)
+        if 1193 <= j <= 1655 and 418 <= i <= 477:
+            mask[i,j] = 1
+        if 1020 <= j <= 1706 and 310 <= i <= 378:
+            mask[i,j] = 1
+        if 1392 <= j <= 1477 and 200 <= i <= 283:
+            mask[i,j] = 1
+        #masking small blooming patch on the central LHS
+        if 770 <= j <= 784:
+            if 3370<= i <=3420 :
+                mask[i,j] = 1
+            if 3200<= i <= 3279:
+                mask[i,j] = 1
+
+#plt.figure()
+#plt.imshow(mask)
+#plt.figure()
+#plt.imshow(data)
 
 #sums values of pixels inside 1st aperture, pixels of galaxy
 def galaxyPhotons(x_centre,y_centre, radius):
 	photon_vals=[];
 	for i in range(x_centre-radius,x_centre+radius):
 		for j in range(y_centre-radius,y_centre+radius):
-			if i<imagex and i>=0 and j<imagey and j>=0: #only consider pixels within the image!
+			if i<imagex and i>=0 and j<imagey and j>=0: #only consider pixels within the image
 				d=np.sqrt((i-x_centre)**2+(j-y_centre)**2)
 				if d<radius: 
-					photon_vals.append(data[j][i])
-			mask[i][j]=1
-	return(np.sum(photon_vals))
+					photon_vals.append(data[i,j])
+				mask[i,j]=1
+	return(np.sum(photon_vals)) #returns total photon counts for the galaxy
 	
 #averages values of pixels between 1st and 2nd aperture, local bakcground mean	
 def localBackground(x_centre,y_centre, initialRadius, secondaryRadius):
 	bg_photon_vals=[];
 	for i in range(x_centre-secondaryRadius,x_centre+secondaryRadius):#outer x
 		for j in range(y_centre-secondaryRadius,y_centre+secondaryRadius):#outer y
-			if i<imagex and i>=0 and j<imagey and j>=0: #only consider pixels within the image!
+			if i<imagex and i>=0 and j<imagey and j>=0: #only consider pixels within the image
 				d2=np.sqrt((i-x_centre)**2+(j-y_centre)**2)
-				if d2<secondaryRadius and d2>initialRadius: #only consider pixels between the two apertures
-					bg_photon_vals.append(data[j][i])
-	return(np.sum(bg_photon_vals)/len(bg_photon_vals))
+				if d2<secondaryRadius and d2>initialRadius: #only consider pixels in between the two apertures
+					bg_photon_vals.append(data[i,j])
+	return(np.sum(bg_photon_vals)/len(bg_photon_vals)) #returns mean local background
 
 def pixelPos(data, value):
 	indices = np.where(data==value) #gives tuples of coordinates
@@ -101,20 +138,20 @@ def pixelPos(data, value):
 	return listIndices
 
 
-initialRadius = 12 #this can be varied as an extension to account for differently sized galaxies
-secondaryRadius=20 #this number needs to be better identified, but for now choose arbitrarily
+initialRadius = 6 #this can be varied as an extension to account for differently sized galaxies
+secondaryRadius=10 #this number needs to be better identified, but for now choose arbitrarily
 galaxyCounts = []
 
 for i in range(len(hist_data_sorted)): 
 	print(i/len(hist_data_sorted)*100)
-	tempPixVal = hist_data_sorted[-(1+i)] #find highest pixel value
+	tempPixVal = hist_data_sorted[-(1+i)] #find next highest pixel value in image
 	if tempPixVal > meanBackground+sigmaBackground: #if the pixel is significantly brighter than background mean, continue
-		tempPixPos = pixelPos(data, tempPixVal) #find position of highest pixel value
-		for j in range(len(tempPixPos)): #potentially multiple locations with same pixel value
+		tempPixPos = pixelPos(data, tempPixVal) #find position of this pixel value
+		for j in range(len(tempPixPos)): #potentially multiple locations with same pixel value, so iterate through them
 			loc = tempPixPos[j]
-			if mask[loc[1]][loc[0]] == 0: #if pixel is available to use 
+			if mask[loc[1],loc[0]] == 0: #if pixel is available to use according to mask
 				galaxyBrightness=galaxyPhotons(loc[1],loc[0],initialRadius)
 				galaxyBackground = localBackground(loc[1],loc[0],initialRadius, secondaryRadius)
 				galaxyCounts.append(galaxyBrightness-galaxyBackground)
 #it would be more efficient to assess mask image earlier, but unsure how to do this
-		
+"""		
